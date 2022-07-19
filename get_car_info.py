f8f3bba2f9c9a06147db1a7bc1fb9cd017aaf905
PakAleksandr <108861005+PakAleksandr@users.noreply.github.com>
2022-07-19 15:30:50 +0600

    Add files via upload

---
+++ get_car_info.py
@@ -0,0 +1,16 @@
+from OOP.Car.create_car import Car
+def get_car_info(model, weight, hp, nm, max_speed, color):
+    car = Car (
+            model=model,
+            weight=weight,
+            hp=hp,
+            nm=nm,
+            max_speed=max_speed,
+            color=color
+    )
+    return car
+
+
+
+
+
